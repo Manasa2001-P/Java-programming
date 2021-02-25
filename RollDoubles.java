f9ec583073187e67e4ae40a89db463508e402e72
Manasa2001-P <70510451+Manasa2001-P@users.noreply.github.com>
2021-02-25 17:29:24 +0530

    Add files via upload

---
+++ RollDoubles.java
@@ -0,0 +1,21 @@
+
+public class RollDoubles
+{
+	public static void main(String args[])
+	{
+		int rolls=10000;
+		int count=0;
+		for(int i=0;i<rolls;i++)
+		{
+			int dice1=1+(int) (Math.random() *10)%6;
+			int dice2=1+(int) (Math.random() *10)%6;
+			if(dice1==dice2)
+			{
+				count+=1;
+			}
+		}
+		System.out.println("count: "+count);
+		
+	}
+}
+			
